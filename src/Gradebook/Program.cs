7e8278855915316eb22609c360cb9c63c565979b
JakeJiwooJeong <jakejiwoojeong@gmail.com>
2019-08-30 23:38:35 -0300

    Uploaded files
    Second synchronization attempt for C# Fundamentals Pluralsight lessons project files.

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hewwo, ny'all!");
+        }
+    }
+}
