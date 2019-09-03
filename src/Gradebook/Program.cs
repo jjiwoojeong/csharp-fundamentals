91b6557318ca1389e65db8e2663aa6c189c49680
JakeJiwooJeong <jakejiwoojeong@gmail.com>
2019-09-03 16:33:43 -0300

    Tried the program by myself
    I wrote up a bunch of code for the program with what I remembered from my Java courses because I got bored.
    
    I should see if it matches up with what the lesson advises me to do.
    
    I do more than I need to in this, I think.

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,14 +7,65 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            Console.WriteLine("[Gradebook 1.0]");
+
+            // A list is more dynamic than an array.
+            // They can still have their coords accessed with 
+            // grades[x] or something similar
+            List<double> grades = new List<double>();
+
+            // This is for storing input double values.
+            double input;
+            // Stores highest grade.
+            double highScore = 0.0;
+            // Stores lowest grade.
+            double lowScore = 100.0;
+            // Stores average grade.
+            double averageScore = 0.0;
+
+            // Introductory spiel.
+            Console.WriteLine("Enter the grade of each student.");
+            Console.WriteLine("Each grade is a floating point number");
+            Console.WriteLine("between 0 to 100, inclusive.");
+            Console.WriteLine("Enter an invalid grade to see the results.");
+            Console.Write("> ");
+
+            // Parse input string as double and store in double input.
+            input = Double.Parse(Console.ReadLine());
+
+            // Loops to gather student grades.
+            while(input >= 0 && input <= 100) 
             {
-                Console.WriteLine($"Hello, {args[0]}!");
+                Console.Write("> ");
+                grades.Add(input);
+                input = Double.Parse(Console.ReadLine());
             }
-            else 
+
+            // First time I've used a foreach. It's very handy!
+            // Used this to add and compare grades for later output.
+            foreach(double grade in grades) 
             {
-                Console.WriteLine("Hello!");
+                averageScore += grade;
+
+                if(grade > highScore)
+                {
+                    highScore = grade;
+                }
+                else if(grade < lowScore) 
+                {
+                    lowScore = grade;
+                }
             }
+
+            // One final operation to divide the average score by the number
+            // of elements in List grades.
+            averageScore /= grades.Count;
+
+            // Final output.
+            Console.WriteLine($"Highest grade: {highScore}");
+            Console.WriteLine($"Lowest grade: {lowScore}");
+            Console.WriteLine($"Average grade: {averageScore}");
+
         }
     }
 }
